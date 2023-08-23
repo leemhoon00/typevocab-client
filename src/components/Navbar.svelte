91543ba81019f242c18704f6d534caaf16d9fd34
leemhoon00 <leemhoon00@naver.com>
2023-08-23 21:36:47 +0900

    navbar, router

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#/">Home</a>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">단어장</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">문제출제</a>
+        </li>
+      </ul>
+      <div class="dropstart" id="profile">
+        <button
+          type="button"
+          class="btn dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          IMG
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Action two</a></li>
+          <li><a class="dropdown-item" href="#">Action three</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<style>
+  #profile {
+    margin-left: auto;
+  }
+
+  .navbar {
+    padding: 0.5rem 1rem;
+  }
+
+  .dropstart .dropdown-toggle::before {
+    content: none;
+  }
+</style>
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import Home from "./pages/Home.svelte";
+
+export default {
+  "/": Home,
+};
