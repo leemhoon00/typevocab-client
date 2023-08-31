6c4d9899cc89a4e0949db0cf63e3c5d8f76f21e2
leemhoon00 <leemhoon00@naver.com>
2023-08-31 21:25:03 +0900

    style: handling exception for api

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,56 +1,86 @@
 import { BACKEND_URL } from "@src/config.js";
 
 export const getUser = async () => {
-  const res = await fetch(`${BACKEND_URL}/user`, {
-    credentials: "include",
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`${BACKEND_URL}/user`, {
+      credentials: "include",
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export const updateUser = async (user) => {
-  const res = await fetch(`${BACKEND_URL}/user`, {
-    method: "PUT",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`${BACKEND_URL}/user`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export const deleteUser = async () => {
-  const res = await fetch(`${BACKEND_URL}/user`, {
-    method: "DELETE",
-    credentials: "include",
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BACKEND_URL}/user`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 // S3에서 이미지 가져오기
 export const getImageUrl = async (url) => {
-  const imgResponse = await fetch(url);
-  const imgBlob = await imgResponse.blob();
-  return URL.createObjectURL(imgBlob);
+  try {
+    const imgResponse = await fetch(url);
+    const imgBlob = await imgResponse.blob();
+    return URL.createObjectURL(imgBlob);
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export const uploadImage = async (file) => {
-  const formData = new FormData();
-  formData.append("file", file);
-  const res = await fetch(`${BACKEND_URL}/user/image`, {
-    method: "POST",
-    credentials: "include",
-    body: formData,
-  });
-  return await res.json();
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    const res = await fetch(`${BACKEND_URL}/user/image`, {
+      method: "POST",
+      credentials: "include",
+      body: formData,
+    });
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export const deleteImage = async () => {
-  const res = await fetch(`${BACKEND_URL}/user/image`, {
-    method: "DELETE",
-    credentials: "include",
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BACKEND_URL}/user/image`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
